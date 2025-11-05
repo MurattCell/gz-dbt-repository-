83a55a440677cd01e4eacaae19241c219dd8d930
Murat Celik <marsixenn@hotmail.com>
2025-11-05 23:25:49 +0000

    3 - dbt ile Staging Modelleri Oluşturma ve Kaynak Dokümantasyonu.new

--- models/staging/raw/stg_raw__sales.sql
+++ models/staging/raw/stg_raw__sales.sql
@@ -1,22 +1,10 @@
-with 
-
-source as (
-
+with source as (
     select * from {{ source('raw', 'sales') }}
-
-),
-
-renamed as (
-
-    select
-        date_date,
-        orders_id,
-        pdt_id,
-        revenue,
-        quantity
-
-    from source
-
 )
-
-select * from renamed
+select
+    date_date,
+    orders_id,
+    pdt_id AS products_id, -- Düzeltme 1: pdt_id -> products_id
+    CAST(revenue AS FLOAT64) AS revenue, -- Bölüm 4 için hazırlık
+    CAST(quantity AS INT64) AS quantity -- Bölüm 4 için hazırlık
+from source
