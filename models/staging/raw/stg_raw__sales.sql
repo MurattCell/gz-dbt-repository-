12ffb231480d428dcb58ed0d6241bd058877ebd6
Murat Celik <marsixenn@hotmail.com>
2025-11-09 20:26:49 +0000

    new

--- models/staging/raw/stg_raw__sales.sql
+++ models/staging/raw/stg_raw__sales.sql
@@ -4,7 +4,7 @@ with source as (
 select
     date_date,
     orders_id,
-    pdt_id AS products_id, -- Düzeltme 1: pdt_id -> products_id
-    CAST(revenue AS FLOAT64) AS revenue, -- Bölüm 4 için hazırlık
-    CAST(quantity AS INT64) AS quantity -- Bölüm 4 için hazırlık
+    pdt_id AS products_id, 
+    CAST(revenue AS FLOAT64) AS revenue, 
+    CAST(quantity AS INT64) AS quantity 
 from source
